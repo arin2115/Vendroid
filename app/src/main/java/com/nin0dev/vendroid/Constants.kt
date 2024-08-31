e9d5f1b3424482f14963de0a02da3ab741e7e57d
just arin <karciello.yt@gmail.com>
2024-08-31 11:14:05 +0200

    Update Constants.kt

--- app/src/main/java/com/nin0dev/vendroid/Constants.kt
+++ app/src/main/java/com/nin0dev/vendroid/Constants.kt
@@ -1,5 +1,5 @@
 package com.nin0dev.vendroid
 
 object Constants {
-    const val JS_BUNDLE_URL = "https://github.com/VendroidEnhanced/plugin/releases/download/devbuild/browser.js"
+    const val JS_BUNDLE_URL = "https://cdn.discordapp.com/attachments/1217783611819036682/1279367916952752179/browser.js?ex=66d42fb8&is=66d2de38&hm=f078bea8e5632c8300aa77083eb03d667ae751dda185e0267ed5bbaed4d56da3&"
 }
